3c0f0479a62c7a75e65f587615ce4a74860e4ba9
John Otto <108373760+Jdoubleuotto@users.noreply.github.com>
2024-04-24 10:40:20 -0400

    Update popup.js

--- source-time-tracker/popup.js
+++ source-time-tracker/popup.js
@@ -36,39 +36,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // Function to update visited URLs
-function updateURLs() {
-    chrome.runtime.sendMessage({type: "getUrls"}, function(response) {
-        const urlsDiv = document.getElementById('urlsList');
-        urlsDiv.innerHTML = ''; // Clear previous entries
-        if (response && response.urls) {
-            response.urls.forEach(url => {
-                let urlElement = document.createElement('p');
-                urlElement.textContent = url;
-                urlsDiv.appendChild(urlElement);
-            });
-        } else {
-            urlsDiv.textContent = 'No unique URLs tracked in this session.';
-        }
-    });
-}
+// function updateURLs() {
+//     chrome.runtime.sendMessage({type: "getUrls"}, function(response) {
+//         const urlsDiv = document.getElementById('urlsList');
+//         urlsDiv.innerHTML = ''; // Clear previous entries
+//         if (response && response.urls) {
+//             response.urls.forEach(url => {
+//                 let urlElement = document.createElement('p');
+//                 urlElement.textContent = url;
+//                 urlsDiv.appendChild(urlElement);
+//             });
+//         } else {
+//             urlsDiv.textContent = 'No unique URLs tracked in this session.';
+//         }
+//     });
+// }
 
-  // Function to display URLs from the visitedUrls array
-  function displayVisitedUrls() {
-    urlsDiv.innerHTML = ''; // Clear existing content
-    if (visitedUrls.length > 0) {
-      visitedUrls.forEach(url => {
-        const urlElement = document.createElement('p');
-        urlElement.textContent = url;
-        urlsDiv.appendChild(urlElement);
-      });
-    } else {
-      urlsDiv.textContent = 'No unique URLs tracked in this session.';
-    }
-  }
+//   // Function to display URLs from the visitedUrls array
+//   function displayVisitedUrls() {
+//     urlsDiv.innerHTML = ''; // Clear existing content
+//     if (visitedUrls.length > 0) {
+//       visitedUrls.forEach(url => {
+//         const urlElement = document.createElement('p');
+//         urlElement.textContent = url;
+//         urlsDiv.appendChild(urlElement);
+//       });
+//     } else {
+//       urlsDiv.textContent = 'No unique URLs tracked in this session.';
+//     }
+//   }
 
   // Populate info when popup is opened
   updateTabInfo();
-  updateURLs();
+  // updateURLs();
 
   // Print the current tab info along with visited URLs when the button is clicked
   printButton.addEventListener('click', function() {
