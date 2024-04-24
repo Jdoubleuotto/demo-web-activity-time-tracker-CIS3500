a280f971674843c040c3e8596ef4a80fedaa1f09
John Otto <108373760+Jdoubleuotto@users.noreply.github.com>
2024-04-24 19:27:41 -0400

    Update popup.js

--- source-time-tracker/popup.js
+++ source-time-tracker/popup.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Update the stored list and display in popup
                     chrome.storage.local.set({visitedUrls: visitedUrls}, function() {
                         console.log('Updated visited URLs:', visitedUrls);
-                        displayVisitedUrls(visitedUrls); // Display visited URLs in the popup
+                        // displayVisitedUrls(visitedUrls); // Display visited URLs in the popup
                         displayUrlTimes(visitedUrls);
                     });
                 });
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Populate the map with time differences between visits
         visitedUrls.forEach((urlInfo, index) => {
             if (!urlMap.has(urlInfo.url)) {
-                urlMap.set(urlInfo.url, 0);
+                urlMap.set(urlInfo.url, []);
             }
             if (index < visitedUrls.length - 1) {
                 const nextVisitTime = new Date(visitedUrls[index + 1].time);
