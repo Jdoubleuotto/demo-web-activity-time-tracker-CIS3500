bcc3a6fb2b669b66f8436d1b6156bed3c29d8239
John Otto <108373760+Jdoubleuotto@users.noreply.github.com>
2024-05-10 16:23:01 -0400

    Update popup.js

--- source-time-tracker/popup.js
+++ source-time-tracker/popup.js
@@ -13,13 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
             let currentTab = tabs[0];
             if (currentTab) {
                 let parsedUrl = new URL(currentTab.url).hostname; // Parse and get the hostname
-                console.log(`Title: ${currentTab.title}\nURL: ${currentTab.url}`);
-                // Use chrome.storage.local to retrieve the current list of URLs
                 infoDiv.textContent = `Title: ${currentTab.title}\nURL: ${parsedUrl}`;
+
                 chrome.storage.local.get({visitedUrls: []}, function(result) {
                     const visitedUrls = result.visitedUrls;
                     visitedUrls.push({title: currentTab.title, url: parsedUrl, time: new Date().toISOString()});
-                    
+
                     chrome.storage.local.set({visitedUrls: visitedUrls}, function() {
                         displayUrlTimesAndUpdateHealth(visitedUrls);
                     });
@@ -28,16 +27,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 infoDiv.textContent = 'No active tab found or URL is not trackable.';
             }
         });
-    }
-
-
-    function parseUrl(url) {
-        let urlObject = new URL(url);
-        return urlObject.hostname; // Returns the domain name without path
-    }
+     }
 
     function displayUrlTimesAndUpdateHealth(visitedUrls) {
         let totalSeconds = 0;
+        let goodTimeSpent = 0;
+        let badTimeSpent = 0;
         const urlMap = new Map();
 
         visitedUrls.forEach((urlInfo, index) => {
@@ -73,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function displayVisitedTimes(urlMap, goodTimeSpent, badTimeSpent) {
-        urlTimes.innerHTML = `<h4>Visited URLs with Total Time Spent:</h4>`;
+        urlTimes.innerHTML = '<h4>Visited URLs with Total Time Spent:</h4>';
         urlMap.forEach((totalSeconds, url) => {
             urlTimes.innerHTML += `<p>URL: ${url}<br>Total Time Spent: ${Math.round(totalSeconds)} seconds</p>`;
         });
