8af40238be23773d88b255406e2ba3012b4a5538
John Otto <108373760+Jdoubleuotto@users.noreply.github.com>
2024-05-11 23:12:38 -0400

    Update background.js

--- source-time-tracker/background.js
+++ source-time-tracker/background.js
@@ -1,25 +1,25 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete' && tab.active) {
+    console.log(`Tab Updated: ${tabId}`, changeInfo, tab);
+    if (changeInfo.status === 'complete' && tab.url && tab.url !== '') {
         let parsedUrl = new URL(tab.url).hostname;
-
-        chrome.storage.local.get({visitedUrls: []}, function(result) {
-            const visitedUrls = result.visitedUrls;
-            visitedUrls.push({title: tab.title, url: parsedUrl, time: new Date().toISOString()});
-
-            chrome.storage.local.set({visitedUrls: visitedUrls});
-        });
+        updateStorageWithVisitedUrl(parsedUrl, tab.title);
     }
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log(`Tab Activated:`, activeInfo);
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-        let parsedUrl = new URL(tab.url).hostname;
-
-        chrome.storage.local.get({visitedUrls: []}, function(result) {
-            const visitedUrls = result.visitedUrls;
-            visitedUrls.push({title: tab.title, url: parsedUrl, time: new Date().toISOString()});
-
-            chrome.storage.local.set({visitedUrls: visitedUrls});
-        });
+        if (tab.url && tab.url !== '') {
+            let parsedUrl = new URL(tab.url).hostname;
+            updateStorageWithVisitedUrl(parsedUrl, tab.title);
+        }
     });
 });
+
+function updateStorageWithVisitedUrl(url, title) {
+    chrome.storage.local.get({visitedUrls: []}, function(result) {
+        const visitedUrls = result.visitedUrls;
+        visitedUrls.push({title: title, url: url, time: new Date().toISOString()});
+        chrome.storage.local.set({visitedUrls: visitedUrls});
+    });
+}
